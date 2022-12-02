2fdbc7a3f157c8d8dbdad1a7e4482f27869e6075
Vladyslav Biriukov <vladislavbiriukovsquirrel@gmail.com>
2022-12-02 22:03:08 +0200

    features header - mobile

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,18 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("backdrop--is-hidden");
-    }
-  })();
+
+  (() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    modalWindow: document.querySelector('[data-modal-window]'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop--is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
